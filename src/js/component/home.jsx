f42d3a055bd7d2a811d23efbf9cef68a99a57032
Camiboro <130386618+Camiboro@users.noreply.github.com>
2024-05-16 00:01:55 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,44 @@
-import React from "react";
-
-//include images into your bundle
+import React, { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [color, setColor] = useState("red");
+
+    const trafficLightContainer = {
+        width: '80px',
+        height: '210px',
+        backgroundColor: '#333',
+        borderRadius: '10px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        padding: '10px 0',
+        margin: '20px auto'
+    };
+
+    const lightStyles = (activeColor) => ({
+        width: '60px',
+        height: '60px',
+        borderRadius: '50%',
+        backgroundColor: activeColor,
+        boxShadow: color === activeColor ? '0 0 15px 5px ' + activeColor : 'none',
+        cursor: 'pointer'
+    });
+
+    return (
+        <div className="text-center mt-5">
+            
+           
+            <div style={trafficLightContainer}>
+                <div onClick={() => setColor('red')} style={lightStyles('red')}></div>
+                <div onClick={() => setColor('yellow')} style={lightStyles('yellow')}></div>
+                <div onClick={() => setColor('green')} style={lightStyles('green')}></div>
+            </div>
+         
+           
+        </div>
+    );
 };
 
 export default Home;
